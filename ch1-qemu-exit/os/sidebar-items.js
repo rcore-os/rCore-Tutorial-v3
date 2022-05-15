56b2975d2c25b04c189bde8d1b5355653712dd4c
chyyuu <chyyuu@users.noreply.github.com>
2022-05-15 09:02:01 +0000

    deploy: 017dbcbd792e182da6d10bebd808db61bae662d5

---
+++ ch1-qemu-exit/os/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["clear_bss","clear BSS segment"],["rust_main","the rust entry-point of os"]],"macro":[["print","print string macro"],["println","println string macro"]],"mod":[["console","SBI console driver, for text output"],["lang_items","The panic handler"],["qemu_exit","RISCV64."],["sbi",""]]});
---
+++ ch1-qemu-exit/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["os"] = {"name":"","files":["console.rs","lang_items.rs","main.rs","qemu_exit.rs","sbi.rs"]};
+createSourceSidebar();
