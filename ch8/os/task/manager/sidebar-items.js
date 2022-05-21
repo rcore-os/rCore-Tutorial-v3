b9f073aaca16bb901714fd5daaa2627188aa7ec2
wyfcyx <wyfcyx@users.noreply.github.com>
2022-05-21 23:00:05 +0000

    deploy: fcfd5b6db7dcceeb35a01bfcc5ef0bcfb2a75054

--- ch8/os/task/manager/sidebar-items.js
+++ ch8/os/task/manager/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["add_task",""],["fetch_task",""],["insert_into_pid2process",""],["pid2process",""],["remove_from_pid2process",""]],"struct":[["PID2PCB",""],["TASK_MANAGER",""],["TaskManager",""]]});
+initSidebarItems({"fn":[["add_task",""],["fetch_task",""],["insert_into_pid2process",""],["pid2process",""],["remove_from_pid2process",""],["remove_task",""]],"struct":[["PID2PCB",""],["TASK_MANAGER",""],["TaskManager",""]]});
--- ch8/os/timer/sidebar-items.js
+++ ch8/os/timer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MSEC_PER_SEC",""],["TICKS_PER_SEC",""]],"fn":[["add_timer",""],["check_timer",""],["get_time",""],["get_time_ms",""],["set_next_trigger",""]],"struct":[["TIMERS",""],["TimerCondVar",""]]});
+initSidebarItems({"constant":[["MSEC_PER_SEC",""],["TICKS_PER_SEC",""]],"fn":[["add_timer",""],["check_timer",""],["get_time",""],["get_time_ms",""],["remove_timer",""],["set_next_trigger",""]],"struct":[["TIMERS",""],["TimerCondVar",""]]});
