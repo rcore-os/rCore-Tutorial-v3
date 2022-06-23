d50317798676a2d5943c7768752b55e4a4f27cbc
chyyuu <chyyuu@users.noreply.github.com>
2022-06-23 12:36:49 +0000

    deploy: 1ac961141629da73742758fd15625a845f97c6b6

---
+++ ch1-dev/os/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["clear_bss","clear BSS segment"],["rust_main","the rust entry-point of os"]],"macro":[["print","print string macro"],["println","println string macro"]],"mod":[["board",""],["console","SBI console driver, for text output"],["lang_items","The panic handler"],["sbi",""]]});
---
+++ ch1-dev/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["os"] = {"name":"","dirs":[{"name":"boards","files":["qemu.rs"]}],"files":["console.rs","lang_items.rs","main.rs","sbi.rs"]};
+createSourceSidebar();
