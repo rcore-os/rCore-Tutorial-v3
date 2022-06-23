16fd7e9e451dd7119a3c37fbe59643d588c8be57
chyyuu <chyyuu@users.noreply.github.com>
2022-06-23 13:52:06 +0000

    deploy: df98d3e1ec5edc361d0d576479e460d8b37636b8

--- ch2/source-files.js
+++ ch2/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["os"] = {"name":"","dirs":[{"name":"sync","files":["mod.rs","up.rs"]},{"name":"syscall","files":["fs.rs","mod.rs","process.rs"]},{"name":"trap","files":["context.rs","mod.rs"]}],"files":["batch.rs","console.rs","lang_items.rs","main.rs","sbi.rs"]};
+sourcesIndex["os"] = {"name":"","dirs":[{"name":"boards","files":["qemu.rs"]},{"name":"sync","files":["mod.rs","up.rs"]},{"name":"syscall","files":["fs.rs","mod.rs","process.rs"]},{"name":"trap","files":["context.rs","mod.rs"]}],"files":["batch.rs","console.rs","lang_items.rs","main.rs","sbi.rs"]};
 createSourceSidebar();
