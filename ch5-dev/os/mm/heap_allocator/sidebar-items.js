aa4ea50555d03433ea8219eec81a5e7d3bcccd13
chyyuu <chyyuu@users.noreply.github.com>
2022-06-23 17:00:55 +0000

    deploy: 395a56fc36f7c14d2ebf27cee0f4064e679dad41

---
+++ ch5-dev/os/mm/heap_allocator/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["handle_alloc_error","panic when heap allocation error occurs"],["heap_test",""],["init_heap","initiate heap allocator"]],"static":[["HEAP_ALLOCATOR","heap allocator instance"],["HEAP_SPACE","heap space ([u8; KERNEL_HEAP_SIZE])"]]});
