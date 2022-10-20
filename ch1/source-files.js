e39ee5135c79d3adf52144cf500c9e52bf52cc2d
YdrMaster <YdrMaster@users.noreply.github.com>
2022-10-20 03:54:45 +0000

    deploy: 66fc99d79f3c14a5a7fadc076d092fe2c1119655

--- ch1/source-files.js
+++ ch1/source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["os"] = {"name":"","dirs":[{"name":"boards","files":["qemu.rs"]}],"files":["console.rs","lang_items.rs","main.rs","sbi.rs"]};
+var sourcesIndex = JSON.parse('{\
+"os":["",[["boards",[],["qemu.rs"]]],["console.rs","lang_items.rs","main.rs","sbi.rs"]]\
+}');
 createSourceSidebar();
