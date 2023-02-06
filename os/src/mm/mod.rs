fa2d574c3b528518b329102efac93b1a4cd88a88
yufeng <321353225@qq.com>
2023-02-06 19:15:46 +0800

    use frame_alloc_more in VirtioHal

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -6,7 +6,7 @@ mod page_table;
 
 use address::VPNRange;
 pub use address::{PhysAddr, PhysPageNum, StepByOne, VirtAddr, VirtPageNum};
-pub use frame_allocator::{frame_alloc, frame_dealloc, FrameTracker};
+pub use frame_allocator::{frame_alloc, frame_alloc_more, frame_dealloc, FrameTracker};
 pub use memory_set::remap_test;
 pub use memory_set::{kernel_token, MapPermission, MemorySet, KERNEL_SPACE};
 use page_table::PTEFlags;
