030e1c23037581155b88aa70f542a3a806bc4d38
chyyuu <chyyuu@users.noreply.github.com>
2023-02-07 16:14:31 +0000

    deploy: 22178ba9b8cd19e0dd1bce5f40b859a4f38044c0

---
+++ main/os/net/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["hexdump",""],["net_interrupt_handler",""]],"mod":[["socket",""],["udp",""]],"struct":[["IPv4",""],["LOSE_NET_STACK",""],["NetStack",""]]};
---
+++ main/os/syscall/net/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["sys_connect",""]]};
