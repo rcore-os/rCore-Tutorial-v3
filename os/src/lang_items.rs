c4e734610c40879406e747c784d28209356197b5
Yifan Wu <shinbokuow@163.com>
2023-03-28 23:42:48 +0800

    Use error! instead of println! when panicking

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -2,18 +2,19 @@ use crate::sbi::shutdown;
 use crate::task::current_kstack_top;
 use core::arch::asm;
 use core::panic::PanicInfo;
+use log::*;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
-        println!(
+        error!(
             "[kernel] Panicked at {}:{} {}",
             location.file(),
             location.line(),
             info.message().unwrap()
         );
     } else {
-        println!("[kernel] Panicked: {}", info.message().unwrap());
+        error!("[kernel] Panicked: {}", info.message().unwrap());
     }
     unsafe {
         backtrace();
