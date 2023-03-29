cf356c606911e509d6736da24bcb6a81d87fbc0f
wyfcyx <wyfcyx@users.noreply.github.com>
2023-03-29 12:08:31 +0000

    deploy: d1caa3f805f680facbce3ce5000e4708d84f96cd

--- ch2/source-files.js
+++ ch2/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"os":["",[["boards",[],["qemu.rs"]],["sync",[],["mod.rs","up.rs"]],["syscall",[],["fs.rs","mod.rs","process.rs"]],["trap",[],["context.rs","mod.rs"]]],["batch.rs","console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]\
+"os":["",[["sync",[],["mod.rs","up.rs"]],["syscall",[],["fs.rs","mod.rs","process.rs"]],["trap",[],["context.rs","mod.rs"]]],["batch.rs","console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]\
 }');
 createSourceSidebar();
