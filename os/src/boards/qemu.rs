7a52aa857bc8f2d7789a1f16efe56ce74e8a7362
Yifan Wu <shinbokuow@163.com>
2023-03-29 20:24:30 +0800

    fix ch3

---
+++ os/src/boards/qemu.rs
@@ -0,0 +1,3 @@
+//! Constants used in rCore for qemu
+
+pub const CLOCK_FREQ: usize = 12500000;
--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -6,11 +6,9 @@ pub fn console_putchar(c: usize) {
     sbi_rt::legacy::console_putchar(c);
 }
 
-/// use sbi call to getchar from console (qemu uart handler)
-#[allow(unused)]
-pub fn console_getchar() -> usize {
-    #[allow(deprecated)]
-    sbi_rt::legacy::console_getchar()
+/// use sbi call to set timer
+pub fn set_timer(timer: usize) {
+    sbi_rt::set_timer(timer as _);
 }
 
 /// use sbi call to shutdown the kernel
