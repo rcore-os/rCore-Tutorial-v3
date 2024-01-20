fdb3f8892fee7a5e86a5bc3641cacd77db5b74bf
wyfcyx <wyfcyx@users.noreply.github.com>
2024-01-20 14:27:19 +0000

    deploy: e1db87049fc3266382d9e1f0a971e9e401b9b577

---
+++ ch5/trait.impl/core/ops/arith/trait.Sub.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"os":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"os/mm/memory_set/struct.MapPermission.html\" title=\"struct os::mm::memory_set::MapPermission\">MapPermission</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
