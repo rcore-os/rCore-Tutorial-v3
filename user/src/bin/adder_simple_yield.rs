b89c12c3484635ed70d8a58f1fdc5ad3084af98a
Yifan Wu <shinbokuow@163.com>
2024-01-21 15:32:33 +0800

    codecheck: Remove UB of ch8 usertests #140

--- user/src/bin/adder_simple_yield.rs
+++ user/src/bin/adder_simple_yield.rs
@@ -1,12 +1,12 @@
 #![no_std]
 #![no_main]
-#![feature(core_intrinsics)]
 
 #[macro_use]
 extern crate user_lib;
 extern crate alloc;
 
 use alloc::vec::Vec;
+use core::ptr::addr_of_mut;
 use user_lib::{exit, get_time, thread_create, waittid, yield_};
 
 static mut A: usize = 0;
@@ -16,7 +16,7 @@ const THREAD_COUNT_DEFAULT: usize = 16;
 static mut PER_THREAD: usize = 0;
 
 unsafe fn critical_section(t: &mut usize) {
-    let a = &mut A as *mut usize;
+    let a = addr_of_mut!(A);
     let cur = a.read_volatile();
     for _ in 0..500 {
         *t = (*t) * (*t) % 10007;
