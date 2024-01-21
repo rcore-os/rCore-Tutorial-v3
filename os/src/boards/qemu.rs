526c7e7d74a05e17d6d7d8130c1353e0083509ad
Yifan Wu <shinbokuow@163.com>
2024-01-21 22:07:42 +0800

    config: use MEMORY_END from mod boards #136

--- os/src/boards/qemu.rs
+++ os/src/boards/qemu.rs
@@ -1,7 +1,7 @@
 //! Constants used in rCore for qemu
 
 pub const CLOCK_FREQ: usize = 12500000;
-pub const MEMORY_END: usize = 0x81000000;
+pub const MEMORY_END: usize = 0x8800_0000;
 
 pub const MMIO: &[(usize, usize)] = &[
     (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
