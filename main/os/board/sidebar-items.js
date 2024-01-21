0e18350cd7a9862fcab90a8addb94714501c657e
wyfcyx <wyfcyx@users.noreply.github.com>
2024-01-21 14:33:21 +0000

    deploy: 56ae7b58ceebc7a80800e7e5d2478ddbe5062f35

--- main/os/board/sidebar-items.js
+++ main/os/board/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["CLOCK_FREQ","MMIO","VIRTGPU_XRES","VIRTGPU_YRES","VIRT_PLIC","VIRT_UART"],"fn":["device_init","irq_handler"],"type":["BlockDeviceImpl","CharDeviceImpl"]};
+window.SIDEBAR_ITEMS = {"constant":["CLOCK_FREQ","MEMORY_END","MMIO","VIRTGPU_XRES","VIRTGPU_YRES","VIRT_PLIC","VIRT_UART"],"fn":["device_init","irq_handler"],"type":["BlockDeviceImpl","CharDeviceImpl"]};
