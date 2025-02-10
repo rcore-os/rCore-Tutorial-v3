73e5765aa6351f351b2e49eae4cbb15c3d074c0d
wyfcyx <wyfcyx@users.noreply.github.com>
2025-02-10 16:07:57 +0000

    deploy: 02c99e8b0866a54bb692210e0b4348fd21f61759

--- ch1/trait.impl/log/trait.Log.js
+++ ch1/trait.impl/log/trait.Log.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"os":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.22/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>"]]
+"os":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.25/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
