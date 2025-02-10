82a817ffd96ff12f2ed0382059b2c33a87b3cbf4
wyfcyx <wyfcyx@users.noreply.github.com>
2025-02-10 16:08:31 +0000

    deploy: 1e82e539d99ef0d4e314e390bd608af22da1b84f

--- ch3/os/sidebar-items.js
+++ ch3/os/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["print","println"],"mod":["board","config","console","lang_items","loader","sbi","sync","syscall","task","timer","trap"]};
+window.SIDEBAR_ITEMS = {"fn":["clear_bss","rust_main"],"macro":["print","println"],"mod":["board","config","console","lang_items","loader","logging","sbi","sync","syscall","task","timer","trap"]};
