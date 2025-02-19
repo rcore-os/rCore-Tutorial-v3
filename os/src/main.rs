00dfff152dfc43602283becc467b534fff2159c9
Yifan Wu <shinbokuow@163.com>
2025-02-19 20:45:19 +0800

    Bump Rust version to nightly-2025-02-18(2024 Edition)

--- os/src/main.rs
+++ os/src/main.rs
@@ -11,7 +11,6 @@
 #![deny(warnings)]
 #![no_std]
 #![no_main]
-#![feature(panic_info_message)]
 
 use core::arch::global_asm;
 use log::*;
@@ -26,35 +25,34 @@ global_asm!(include_str!("entry.asm"));
 
 /// clear BSS segment
 pub fn clear_bss() {
-    extern "C" {
-        fn sbss();
-        fn ebss();
+    unsafe extern "C" {
+        safe fn sbss();
+        safe fn ebss();
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
 /// the rust entry-point of os
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack_lower_bound(); // stack lower bound
-        fn boot_stack_top(); // stack top
+    unsafe extern "C" {
+        safe fn stext(); // begin addr of text segment
+        safe fn etext(); // end addr of text segment
+        safe fn srodata(); // start addr of Read-Only data segment
+        safe fn erodata(); // end addr of Read-Only data ssegment
+        safe fn sdata(); // start addr of data segment
+        safe fn edata(); // end addr of data segment
+        safe fn sbss(); // start addr of BSS segment
+        safe fn ebss(); // end addr of BSS segment
+        safe fn boot_stack_lower_bound(); // stack lower bound
+        safe fn boot_stack_top(); // stack top
     }
     clear_bss();
     logging::init();
     println!("[kernel] Hello, world!");
     trace!(
         "[kernel] .text [{:#x}, {:#x})",
-        stext as usize,
-        etext as usize
+        stext as usize, etext as usize
     );
     debug!(
         "[kernel] .rodata [{:#x}, {:#x})",
