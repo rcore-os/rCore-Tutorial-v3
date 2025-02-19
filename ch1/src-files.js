480fd07c975a13d0a62db9c08fa926435ad1ced3
wyfcyx <wyfcyx@users.noreply.github.com>
2025-02-19 12:47:15 +0000

    deploy: 9a36e8ab78555ed4e81f6faa1eb879ccd5895c32

--- ch1/src-files.js
+++ ch1/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["os",["",[],["console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["os",["",[],["console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[77]}
--- ch1/trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
+++ ch1/trait.impl/core/panic/unwind_safe/trait.RefUnwindSafe.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",1,["os::console::Stdout"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>",1,["os::logging::SimpleLogger"]]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"os/console/struct.Stdout.html\" title=\"struct os::console::Stdout\">Stdout</a>",1,["os::console::Stdout"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"os/logging/struct.SimpleLogger.html\" title=\"struct os::logging::SimpleLogger\">SimpleLogger</a>",1,["os::logging::SimpleLogger"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[692]}
