894637f0b5f67704ce9148550825bb209691e7f4
wyfcyx <wyfcyx@users.noreply.github.com>
2025-02-26 14:42:04 +0000

    deploy: d2a33f8870fdfefe74b4f3a8161973c6356eb0ff

--- ch5/trait.impl/core/ops/bit/trait.Not.js
+++ ch5/trait.impl/core/ops/bit/trait.Not.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"os/mm/memory_set/struct.MapPermission.html\" title=\"struct os::mm::memory_set::MapPermission\">MapPermission</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"os/mm/page_table/struct.PTEFlags.html\" title=\"struct os::mm::page_table::PTEFlags\">PTEFlags</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"os/mm/struct.MapPermission.html\" title=\"struct os::mm::MapPermission\">MapPermission</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[546]}
