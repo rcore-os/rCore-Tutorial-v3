4596db0face2657b0f22c3bb3ae41e5fddd8c118
wyfcyx <wyfcyx@users.noreply.github.com>
2025-04-04 08:05:38 +0000

    deploy: a4525b7907886df57fea48d0f77f15563d1120fb

--- ch7/trait.impl/core/default/trait.Default.js
+++ ch7/trait.impl/core/default/trait.Default.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalAction.html\" title=\"struct os::task::action::SignalAction\">SignalAction</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalActions.html\" title=\"struct os::task::action::SignalActions\">SignalActions</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalAction.html\" title=\"struct os::task::action::SignalAction\">SignalAction</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"os/task/action/struct.SignalActions.html\" title=\"struct os::task::action::SignalActions\">SignalActions</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[595]}
