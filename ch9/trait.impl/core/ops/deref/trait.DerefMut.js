7ebd60b562039f8be125a38a92d8046d4990c232
wyfcyx <wyfcyx@users.noreply.github.com>
2025-04-04 08:05:46 +0000

    deploy: aa8f9c09388e2b4c317c7ba02c22d54b5beaa750

--- ch9/trait.impl/core/ops/deref/trait.DerefMut.js
+++ ch9/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"os/sync/up/struct.UPIntrRefMut.html\" title=\"struct os::sync::up::UPIntrRefMut\">UPIntrRefMut</a>&lt;'a, T&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"os/sync/up/struct.UPIntrRefMut.html\" title=\"struct os::sync::up::UPIntrRefMut\">UPIntrRefMut</a>&lt;'a, T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[326]}
