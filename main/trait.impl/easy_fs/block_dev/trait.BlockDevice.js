a8faa3de3b0926769cfde96e19ac19561cac60f5
wyfcyx <wyfcyx@users.noreply.github.com>
2025-04-04 08:05:53 +0000

    deploy: a78c4f343f7fb693d43c024ebb8073187ddd9230

--- main/trait.impl/easy_fs/block_dev/trait.BlockDevice.js
+++ main/trait.impl/easy_fs/block_dev/trait.BlockDevice.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl BlockDevice for <a class=\"struct\" href=\"os/drivers/block/virtio_blk/struct.VirtIOBlock.html\" title=\"struct os::drivers::block::virtio_blk::VirtIOBlock\">VirtIOBlock</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl BlockDevice for <a class=\"struct\" href=\"os/drivers/block/virtio_blk/struct.VirtIOBlock.html\" title=\"struct os::drivers::block::virtio_blk::VirtIOBlock\">VirtIOBlock</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[191]}
--- main/trait.impl/virtio_drivers/hal/trait.Hal.js
+++ main/trait.impl/virtio_drivers/hal/trait.Hal.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"os":[["impl Hal for <a class=\"struct\" href=\"os/drivers/bus/virtio/struct.VirtioHal.html\" title=\"struct os::drivers::bus::virtio::VirtioHal\">VirtioHal</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["os",[["impl Hal for <a class=\"struct\" href=\"os/drivers/bus/virtio/struct.VirtioHal.html\" title=\"struct os::drivers::bus::virtio::VirtioHal\">VirtioHal</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[165]}
